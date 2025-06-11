1d8686386cae87f34e0e60eeee7ad94430efcdb7
smml-espiritu <techstacks.sam@gmail.com>
2025-06-11 11:07:50 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -7,6 +7,5 @@ const COOLDOWN_SECONDS = 30;
 
 if (!TOKEN) throw new Error("Missing DISCORD_BOT_TOKEN environment variable");
 if (!CLIENT_ID) throw new Error("Missing CLIENT_ID environment variable");
-if (!GUILD_ID) throw new Error("Missing GUILD_ID environment variable");
 
 module.exports = { TOKEN, CLIENT_ID, COOLDOWN_SECONDS };
